90081fca987824880ffc27bc512e2b698690b1cf
paulclifford <clifford@queensu.ca>
2020-05-05 17:01:02 -0400

    add analog clock

--- src/components/homepage/Hours.jsx
+++ src/components/homepage/Hours.jsx
@@ -1,9 +1,22 @@
 import React from "react"
+import AnalogClock from 'analog-clock-react';
+
+let options = {
+  width: "120px",
+  border: false,
+  borderColor: "#406486",
+  baseColor: "#fff",
+  centerColor: "#406486",
+  handColors: {
+    second: "#b81237",
+    minute: "#002f5e",
+    hour: "#febe10"
+  }
+};
 
 const Hours = () => (
   <section className="hours">
-    Hours go here
-    <section className="clock">clock</section>
+    <section className="clock"><AnalogClock {...options} className="qul-clock"/></section>
     <section className="todays-hours">todays-hours</section>
     <section className="people-counter">people-counter</section>
   </section>
