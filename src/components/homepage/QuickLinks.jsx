f7e46c3eeebf7b2358037553cadc9f88056d81a1
paulclifford <clifford@queensu.ca>
2020-05-07 11:42:55 -0400

    homepage quicklinks and related graphics

--- src/components/homepage/QuickLinks.jsx
+++ src/components/homepage/QuickLinks.jsx
@@ -1,7 +1,75 @@
 import React from "react"
+import { Link } from 'gatsby'
+
+import LinkSymbol from '../../images/link-symbol-gold.svg'
+
 
 const QuickLinks = () => (
-  <nav className="quick-links">QuickLinks here</nav>
+  <nav className="quick-links">
+    <section>
+      <h2>Using the Library</h2>
+      <ul>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h2>Learning</h2>
+      <ul>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h2>Research</h2>
+      <ul>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+        <li>
+          <Link to="/">link</Link>
+        </li>
+      </ul>
+    </section>
+    <section className="link-symbol">
+      <img src={LinkSymbol} width="" height="" alt="quick links section" />
+    </section>
+  </nav>
 )
 
 export default QuickLinks
