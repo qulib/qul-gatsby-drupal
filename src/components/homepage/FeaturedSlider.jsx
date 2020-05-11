650a192a031e2c0a93daacbc9b00493a8183a454
Paul Clifford <clifford@queensu.ca>
2020-05-11 14:49:24 -0400

    work on featured slider

--- src/components/homepage/FeaturedSlider.jsx
+++ src/components/homepage/FeaturedSlider.jsx
@@ -1,41 +1,93 @@
-import React from "react"
-import Slider from 'react-slick'
-// see https://react-slick.neostack.com/
-
+import React, { Component } from 'react'
+import { Carousel } from 'antd'
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
 import BellSymbol from '../../images/bell-symbol.svg'
 
-const settings = {
-  dots: true,
-  fade: true,
-  lazyLoad: true,
-  infinite: true,
-  pauseOnHover: true,
-  speed: 500,
-  autoplaySpeed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1
+// see https://react-slick.neostack.com/
+// const settings = {
+//   dots: true,
+//   fade: true,
+//   lazyLoad: true,
+//   infinite: true,
+//   pauseOnHover: true,
+//   speed: 500,
+//   autoplaySpeed: 500,
+//   slidesToShow: 1,
+//   slidesToScroll: 1
+// }
+
+class FeaturedSlider extends Component {
+  constructor(props) {
+    super(props)
+    this.next = this.next.bind(this)
+    this.previous = this.previous.bind(this)
+    this.carousel = React.createRef()
+  }
+
+  next() {
+    this.carousel.next()
+  }
+  previous() {
+    this.carousel.prev()
+  }
+
+  render() {
+    const props = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    }
+    return (
+      <section className="featured-slider">
+        <section className="slider">
+          <button className="slider-nav prev" onClick={this.previous}>
+            <BsChevronLeft />
+          </button>
+          <button className="slider-nav next" onClick={this.next}>
+            <BsChevronRight />
+          </button>
+
+          <Carousel ref={node => (this.carousel = node)} {...props}>
+            <article className="slide">
+              <h2>Did You Know 1</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Sapiente eius nihil hic, magnam voluptatem numquam soluta.
+              </p>
+              <button>Read More</button>
+            </article>
+            <article className="slide">
+              <h2>Did You Know 2</h2>
+              <p>
+                Dolor!, sit amet consectetur adipisicing elit. Sapiente eius
+                nihil hic, magnam voluptatem numquam soluta.
+              </p>
+              <button>Read More</button>
+            </article>
+            <article className="slide">
+              <h2>Did You Know 3</h2>
+              <p>
+                Freedddddeeed m soluta.
+              </p>
+              <button>Read More</button>
+            </article>
+          </Carousel>
+
+        </section>
+
+        <section className="bell-symbol">
+          <img
+            src={BellSymbol}
+            width=""
+            height=""
+            alt="featured slider section"
+          />
+        </section>
+      </section>
+    )
+  }
 }
-
-const FeaturedSlider = () => (
-  <section className="featured-slider">
-
-    <Slider {...settings}>
-      <article className="slide">
-        <h2>Did You Know 1</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente eius nihil hic, magnam voluptatem numquam soluta.</p>
-        <button>Read More</button>
-      </article>
-      <article className="slide">
-        <h2>Did You Know 2</h2>
-        <p>Dolor!, sit amet consectetur adipisicing elit. Sapiente eius nihil hic, magnam voluptatem numquam soluta.</p>
-        <button>Read More</button>
-      </article>
-    </Slider>
-
-    <section className="bell-symbol">
-      <img src={BellSymbol} width="" height="" alt="featured slider section" />
-    </section>
-  </section>
-)
 
 export default FeaturedSlider
