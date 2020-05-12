57be5a4a7c66b7081448975d3bd0e8a163dd787f
paulclifford <clifford@queensu.ca>
2020-05-12 14:10:23 -0400

    basic page, ask us box

--- src/templates/basic-page.jsx
+++ src/templates/basic-page.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 // import Img from 'gatsby-image'
+// import { Link } from 'gatsby'
+import AskUsWidget from '../components/homepage/AskUsWidget.jsx'
 
 import Layout from '../components/Layout.jsx'
 import Breadcrumbs from '../components/global/Breadcrumbs.jsx'
@@ -17,27 +19,31 @@ class PageTemplate extends Component {
       <Layout>
         <div id="site-body" className="container">
           <Helmet>
-            <title>Queen's Univesity Library - {post.title}</title>
+            <title>QUL - {post.title}</title>
           </Helmet>
           <Breadcrumbs />
 
           <section className="basic-page">
+
             <main className="content">
               <h1>{post.title}</h1>
               <div dangerouslySetInnerHTML={{ __html: post.body.processed }} />
             </main>
 
             <aside className="sidebar">
-              <section>
+
+              <section className="ask-us-box">
                 <h2>Ask Us</h2>
                 <p>
                   For help locating resources, using the library, or to request
                   a research consultation, try our Ask Us service.
                 </p>
-                <button>Button</button>
+
+                <AskUsWidget />
               </section>
 
               {/* if sidebar content exists in Drupal node, then optionally display here */}
+
             </aside>
           </section>
         </div>
