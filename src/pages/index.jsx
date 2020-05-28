5461be627d82e1a00834068f9fb78cbe6bc83d9e
Paul Clifford <clifford@queensu.ca>
2020-05-28 15:22:21 -0400

    made homepage self-reliant

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
-
-import HomepageLayout from '../components/HomepageLayout.jsx'
+import Header from '../components/header/Header'
+import Footer from '../components/footer/Footer'
+import { siteTitle } from '../components/Layout.jsx'
 // import Banner from '../components/homepage/Banner.jsx'
 import OmniWidget from '../components/homepage/OmniWidget.jsx'
 import AskUsWidget from '../components/homepage/AskUsWidget.jsx'
@@ -10,73 +11,82 @@ import Hours from '../components/homepage/Hours.jsx'
 import QuickLinks from '../components/homepage/QuickLinks.jsx'
 import NewsEvents from '../components/homepage/NewsEvents.jsx'
 import FeaturedSlider from '../components/homepage/FeaturedSlider.jsx'
-
-const pageTitle = 'Home'
+import 'typeface-open-sans'
+import '../styles/style.scss'
 
 class Home extends React.Component {
   render() {
     const data = this.props.data
-    const siteTitle = data.site.siteMetadata.title
     const newsEventsData = data.allNodeNewsEvents
-    // console.log(newsEventsData)
 
     return (
-      <HomepageLayout>
-        <Helmet>
-          <title>{siteTitle} - {pageTitle}</title>
+      <div id="site">
+        <Helmet htmlAttributes={{ lang: 'en' }}>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          ></meta>
+          <title>{siteTitle}</title>
         </Helmet>
-        <h1>Home</h1>
-        {/* <Banner /> */}
-        <section className="omni-ask-us">
-          <OmniWidget />
-          <AskUsWidget />
-        </section>
-        <Hours />
-        <QuickLinks />
-        <NewsEvents data={newsEventsData} />
-        <FeaturedSlider />
-      </HomepageLayout>
+        <Header />
+        <div id="home" className="container">
+          <h1>Home</h1>
+          {/* <Banner /> */}
+          <section className="omni-ask-us">
+            <OmniWidget />
+            <AskUsWidget />
+          </section>
+          <Hours />
+          <QuickLinks />
+          <NewsEvents data={newsEventsData} />
+          <FeaturedSlider />
+        </div>
+        <Footer />
+      </div>
     )
   }
 }
 
 export default Home
 
-
-
 export const query = graphql`
-query Homepage {
-  site {
-    siteMetadata {
-      title
-    }
-  }
-  allNodeNewsEvents(filter: {field_promote_to_homepage: {eq: true}}, limit: 6, sort: {fields: created, order: DESC}) {
-    edges {
-      node {
+  query Homepage {
+    site {
+      siteMetadata {
         title
-        promote
-        path {
-          alias
-        }
-        drupal_internal__nid
-        created
-        relationships {
-          field_featured_image {
-            localFile {
-              childImageSharp {
-                fluid(maxWidth: 500, maxHeight: 300) {
-                  ...GatsbyImageSharpFluid
+      }
+    }
+    allNodeNewsEvents(
+      filter: { field_promote_to_homepage: { eq: true } }
+      limit: 6
+      sort: { fields: created, order: DESC }
+    ) {
+      edges {
+        node {
+          title
+          promote
+          path {
+            alias
+          }
+          drupal_internal__nid
+          created
+          relationships {
+            field_featured_image {
+              localFile {
+                childImageSharp {
+                  fluid(maxWidth: 500, maxHeight: 300) {
+                    ...GatsbyImageSharpFluid
+                  }
                 }
               }
             }
           }
-        }
-        field_featured_image {
-          alt
+          field_featured_image {
+            alt
+          }
         }
       }
     }
   }
-}
 `
