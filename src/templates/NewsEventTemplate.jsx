bbf8720b30d3e10898b0aa8f906f255959739f08
paulclifford <clifford@queensu.ca>
2020-06-01 12:31:23 -0400

    rewrite News & Events using React Hooks

--- src/templates/NewsEventTemplate.jsx
+++ src/templates/NewsEventTemplate.jsx
@@ -1,50 +1,54 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
-// import Img from 'gatsby-image'
+import Img from 'gatsby-image'
 // import { Link } from 'gatsby'
 import Layout from '../components/Layout.jsx'
 import Breadcrumbs from '../components/global/Breadcrumbs.jsx'
-// import AskUsWidget from '../components/homepage/AskUsWidget.jsx'
+import AskUsWidget from '../components/homepage/AskUsWidget.jsx'
 
-// add error checking to data assignments
+function NewsEventTemplate({ data }) {
+  const post = data.nodeNewsEvents
 
-class NewsEventTemplate extends React.Component {
-  render() {
-    const post = this.props.data.nodeNewsEvents
-    // console.log(post)
+  return (
+    <Layout>
+      <Helmet>
+        <title>QUL - {post.title}</title>
+      </Helmet>
+      <Breadcrumbs />
+      <div className="news-event-page">
+        <main className="content">
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.body.processed }} />
+        </main>
 
-    return (
-      <Layout>
+        <aside className="sidebar">
+          <section className="ask-us-box">
+            <h2>Ask Us</h2>
+            <p>
+              For help locating resources, using the library, or to request a
+              research consultation, try our Ask Us service.
+            </p>
 
-        <Helmet>
-          <title>QUL - {post.title}</title>
-        </Helmet>
+            <AskUsWidget />
+          </section>
 
-        <Breadcrumbs />
-
-        <div className="news-event-page">
-
-          <main className="content">
-            <h1>{post.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.body.processed }} />
-          </main>
-        </div>
-
-      </Layout>
-    )
-  }
+          {/* if sidebar content exists in Drupal node, then optionally display here */}
+        </aside>
+      </div>
+    </Layout>
+  )
 }
 
 export default NewsEventTemplate
 
 export const pageQuery = graphql`
-query($id: Int!) {
-  nodeNewsEvents(drupal_internal__nid: { eq: $id }) {
-    title
-    body {
-      processed
+  query($id: Int!) {
+    nodeNewsEvents(drupal_internal__nid: { eq: $id }) {
+      title
+      body {
+        processed
+      }
     }
   }
-}
 `
