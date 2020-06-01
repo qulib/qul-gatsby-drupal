3114029f36a9e58c9a9f25eb50ddc0dcd573e492
paulclifford <clifford@queensu.ca>
2020-06-01 12:30:58 -0400

    rewrite basic page using React Hooks

--- src/templates/BasicPageTemplate.jsx
+++ src/templates/BasicPageTemplate.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+// import ReactHtmlParser from 'react-html-parser' // need to check for inline images
+// see: https://gist.github.com/andrewl/e868f2afb2386aadf711da35bc4c825b
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 // import Img from 'gatsby-image'
@@ -8,47 +10,38 @@ import Breadcrumbs from '../components/global/Breadcrumbs.jsx'
 import AskUsWidget from '../components/homepage/AskUsWidget.jsx'
 
 // add error checking to data assignments
+function BasicPageTemplate({ data }) {
+  // console.log('data is: ', data)
+  const post = data.nodePage
 
-class BasicPageTemplate extends React.Component {
-  render() {
-    const post = this.props.data.nodePage
-    // console.log(post)
+  return (
+    <Layout>
+      <Helmet>
+        <title>QUL - {post.title}</title>
+      </Helmet>
+      <Breadcrumbs />
+      <div className="basic-page">
+        <main className="content">
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.body.processed }} />
+        </main>
 
-    return (
-      <Layout>
+        <aside className="sidebar">
+          <section className="ask-us-box">
+            <h2>Ask Us</h2>
+            <p>
+              For help locating resources, using the library, or to request a
+              research consultation, try our Ask Us service.
+            </p>
 
-        <Helmet>
-          <title>QUL - {post.title}</title>
-        </Helmet>
+            <AskUsWidget />
+          </section>
 
-        <Breadcrumbs />
-
-        <div className="basic-page">
-
-          <main className="content">
-            <h1>{post.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.body.processed }} />
-          </main>
-
-          <aside className="sidebar">
-            <section className="ask-us-box">
-              <h2>Ask Us</h2>
-              <p>
-                For help locating resources, using the library, or to request
-                a research consultation, try our Ask Us service.
-                </p>
-
-              <AskUsWidget />
-            </section>
-
-            {/* if sidebar content exists in Drupal node, then optionally display here */}
-
-          </aside>
-        </div>
-
-      </Layout>
-    )
-  }
+          {/* if sidebar content exists in Drupal node, then optionally display here */}
+        </aside>
+      </div>
+    </Layout>
+  )
 }
 
 export default BasicPageTemplate
