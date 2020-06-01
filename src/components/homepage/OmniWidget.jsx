5435d418ca99fc93327d107720e29f1966d6630c
paulclifford <clifford@queensu.ca>
2020-06-01 12:30:29 -0400

    a11y updates to Omni widget

--- src/components/homepage/OmniWidget.jsx
+++ src/components/homepage/OmniWidget.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Input, Tooltip, Button } from 'antd';
-import { FaQuestionCircle } from "react-icons/fa";
-import { AiOutlineSearch } from "react-icons/ai";
+import { Input, Tooltip, Button } from 'antd'
+import { FaQuestionCircle } from 'react-icons/fa'
+import { AiOutlineSearch } from 'react-icons/ai'
 import OmniSymbol from '../../images/qul-omni-logo.svg'
 
 const OmniTooltip = (
-  <span className="omni-tooltip">Find full-text articles, books and ebooks, journals, media, and more in a single search</span>
+  <span className="omni-tooltip">
+    Find full-text articles, books and ebooks, journals, media, and more in a
+    single search
+  </span>
 )
 
-const { Search } = Input;
+const { Search } = Input
 
 const OmniSearchButton = (
-  <Button className="ant-btn ant-input-search-button ant-btn-primary" type="primary" htmlType="submit"><AiOutlineSearch /></Button>
+  <Button
+    className="ant-btn ant-input-search-button ant-btn-primary"
+    type="primary"
+    htmlType="submit"
+  >
+    <AiOutlineSearch />
+  </Button>
 )
 
 class OmniWidget extends React.Component {
@@ -22,17 +31,22 @@ class OmniWidget extends React.Component {
   }
 
   searchPrimo() {
-    document.getElementById("primoQuery").value = "any,contains," + document.getElementById("primoQueryTemp").value.replace(/[,]/g, " ")
-    document.forms["searchForm"].submit()
+    document.getElementById('primoQuery').value =
+      'any,contains,' +
+      document.getElementById('primoQueryTemp').value.replace(/[,]/g, ' ')
+    document.forms['searchForm'].submit()
   }
 
   render() {
     return (
       <section className="omni-widget">
-
         <section className="omni-branding">
           <img src={OmniSymbol} width="200" height="62" alt="Omni logo" />
-          <Tooltip placement="right" title={OmniTooltip} overlayClassName="omni-tooltip">
+          <Tooltip
+            placement="right"
+            title={OmniTooltip}
+            overlayClassName="omni-tooltip"
+          >
             <FaQuestionCircle />
           </Tooltip>
         </section>
@@ -47,23 +61,80 @@ class OmniWidget extends React.Component {
             onSubmit={this.searchPrimo}
             target="_self"
           >
-            <input name="institution" type="hidden" value="01OCUL_QU" />
-            <input name="vid" type="hidden" value="01OCUL_QU:QU_DEFAULT" />
-            <input id="primoTab" name="tab" type="hidden" value="Everything" />
+            <input
+              name="institution"
+              type="hidden"
+              value="01OCUL_QU"
+              aria-label="Omni control"
+            />
+            <input
+              name="vid"
+              type="hidden"
+              value="01OCUL_QU:QU_DEFAULT"
+              aria-label="Omni control"
+            />
+            <input
+              id="primoTab"
+              name="tab"
+              type="hidden"
+              value="Everything"
+              aria-label="Omni control"
+            />
             <input
               id="primoScope"
               name="search_scope"
               type="hidden"
               value="MyInst_and_CI"
+              aria-label="Omni control"
             />
-            <input name="mode" type="hidden" value="Basic" />
-            <input name="displayMode" type="hidden" value="full" />
-            <input name="bulkSize" type="hidden" value="10" />
-            <input name="highlight" type="hidden" value="true" />
-            <input name="dum" type="hidden" value="true" />
-            <input id="primoQuery" name="query" type="hidden" />
-            <input name="displayField" type="hidden" value="all" />
-            <input name="pcAvailabiltyMode" type="hidden" value="true" />
+            <input
+              name="mode"
+              type="hidden"
+              value="Basic"
+              aria-label="Omni control"
+            />
+            <input
+              name="displayMode"
+              type="hidden"
+              value="full"
+              aria-label="Omni control"
+            />
+            <input
+              name="bulkSize"
+              type="hidden"
+              value="10"
+              aria-label="Omni control"
+            />
+            <input
+              name="highlight"
+              type="hidden"
+              value="true"
+              aria-label="Omni control"
+            />
+            <input
+              name="dum"
+              type="hidden"
+              value="true"
+              aria-label="Omni control"
+            />
+            <input
+              id="primoQuery"
+              name="query"
+              type="hidden"
+              aria-label="Omni control"
+            />
+            <input
+              name="displayField"
+              type="hidden"
+              value="all"
+              aria-label="Omni control"
+            />
+            <input
+              name="pcAvailabiltyMode"
+              type="hidden"
+              value="true"
+              aria-label="Omni control"
+            />
             <Search
               id="primoQueryTemp"
               placeholder="Search anything"
@@ -75,10 +146,14 @@ class OmniWidget extends React.Component {
 
         <ul className="omni-links">
           <li>
-            <a href="https://ocul-qu.primo.exlibrisgroup.com/discovery/search?vid=01OCUL_QU:QU_DEFAULT&amp;lang=en&amp;mode=advanced">Advanced Search</a>
+            <a href="https://ocul-qu.primo.exlibrisgroup.com/discovery/search?vid=01OCUL_QU:QU_DEFAULT&amp;lang=en&amp;mode=advanced">
+              Advanced Search
+            </a>
           </li>
           <li>
-            <a href="https://ocul-qu.primo.exlibrisgroup.com/discovery/jsearch?vid=01OCUL_QU:QU_DEFAULT&amp;lang=en">Journals</a>
+            <a href="https://ocul-qu.primo.exlibrisgroup.com/discovery/jsearch?vid=01OCUL_QU:QU_DEFAULT&amp;lang=en">
+              Journals
+            </a>
           </li>
           <li>
             <Link to="/search/databases/browse">Databases</Link>
@@ -87,8 +162,7 @@ class OmniWidget extends React.Component {
             <Link to="/search/omni">Help</Link>
           </li>
         </ul>
-
-      </section >
+      </section>
     )
   }
 }
