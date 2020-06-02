f3f87574c58e4db476028be7a4b216abd434c588
paulclifford <clifford@queensu.ca>
2020-06-02 15:30:48 -0400

    all filters and pagination working together

--- src/pages/about-us/news-events.jsx
+++ src/pages/about-us/news-events.jsx
@@ -1,162 +1,138 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 import { Input, Select, Pagination } from 'antd'
 import Layout, { siteTitle } from '../../components/Layout.jsx'
 import Breadcrumbs from '../../components/global/Breadcrumbs.jsx'
 import NewsCard from '../../components/global/NewsCard.jsx'
-// import { node } from 'prop-types'
 
 const { Search } = Input
 const { Option } = Select
 const pageTitle = 'News & Events'
-const cardsPerPage = 3
+const cardsPerPage = 6
 
-class AllNewsEventsPage extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      category: '',
-      search: '',
-      page: '1',
-    }
-    this.updateSearch = this.updateSearch.bind(this)
-    this.updateCategory = this.updateCategory.bind(this)
-    this.updatePage = this.updatePage.bind(this)
-    this.paginate = this.paginate.bind(this)
+function AllNewsEventsPage({ data }) {
+  const [category, setCategory] = useState('')
+  const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1)
+
+  function updateSearch(value) {
+    setSearch(value)
+    setPage(1)
   }
 
-  updateSearch(value) {
-    this.setState({
-      search: value,
-      page: '1', // need to reset pagination somehow
-    })
+  function updateCategory(value) {
+    setCategory(value)
+    setPage(1)
   }
 
-  updateCategory(value) {
-    this.setState({ category: value })
-  }
-
-  updatePage(value) {
-    this.setState({ page: value })
-  }
-
-  paginate(pages) {
-    const begin = (this.state.page - 1) * cardsPerPage
+  function paginate(pages) {
+    const begin = (page - 1) * cardsPerPage
     // check for overflow at end
     const end =
       begin + cardsPerPage < pages.length ? begin + cardsPerPage : pages.length
     return pages.slice(begin, end)
   }
 
-  render() {
-    const data = this.props.data
-    let filteredNewsEvents = data.allNodeNewsEvents.edges //unfiltered
-
-    // create category filter
-    const categories = []
-    data.allTaxonomyTermNewsEventsCategory.edges.forEach(({ node }) => {
-      categories.push(
-        <Option
-          key={node.drupal_internal__tid}
-          value={node.drupal_internal__tid}
-        >
-          {node.name}
-        </Option>
-      )
-    })
-
-    // title search filter
-    filteredNewsEvents = filteredNewsEvents.filter(edge => {
-      return (
-        edge.node.title
-          .toLowerCase()
-          .indexOf(this.state.search.toLowerCase()) !== -1
-      )
-    })
-
-    // category filter
-    if (this.state.category) {
-      console.log('cat is: ', this.state.category)
-
-      // console.log(node.relationships.field_category)
-
-      // filteredNewsEvents.filter(node => node.relationships.field_category.drupal_internal__tid === this.state.category)
-
-      // close ! see" https://stackoverflow.com/questions/8217419/how-to-determine-if-javascript-array-contains-an-object-with-an-attribute-that-e/8217584#8217584
-
-      // if (filteredNewsEvents.some(e => e.Name === this.state.category)) {
-      //   /* vendors contains the element we're looking for */
-      // }
-    }
-
-    //pagination
-    const pagedNewsEvents = this.paginate(filteredNewsEvents)
-    // console.log(pagedNewsEvents)
-
-    return (
-      <Layout>
-        <Helmet>
-          <title>
-            {siteTitle} - {pageTitle}
-          </title>
-        </Helmet>
-
-        <Breadcrumbs />
-
-        <header className="full-width-header">
-            <h1>{pageTitle}</h1>
-
-          <section className="filters">
-            <section className="category-filter">
-              <Select
-                placeholder="Category"
-                style={{ width: '100%' }}
-                onChange={this.updateCategory}
-                allowClear
-              >
-                {categories}
-              </Select>
-            </section>
-
-            <section className="search-filter">
-              <Search
-                placeholder="Title"
-                allowClear
-                enterButton
-                onChange={e => this.updateSearch(e.target.value)}
-                onSearch={value => this.updateSearch(value)}
-              />
-            </section>
-          </section>
-
-          <section className="filter-results">
-              <Pagination
-                total={filteredNewsEvents.length}
-                current={this.state.page}
-                showTotal={(total, range) =>
-                  `${range[0]}-${range[1]} of ${total} items`
-                }
-                pageSize={cardsPerPage}
-                className="pagination"
-                onChange={page => this.updatePage(page)}
-              />
-            </section>
-        </header>
-
-        <main className="news-events-page">
-          <section className="news-grid">
-            {pagedNewsEvents.map(({ node }) => (
-              <NewsCard
-                key={node.drupal_internal__nid.toString()}
-                node={node}
-              />
-            ))}
-          </section>
-        </main>
-      </Layout>
+  // create category filter
+  const categories = []
+  data.allTaxonomyTermNewsEventsCategory.edges.forEach(({ node }) => {
+    categories.push(
+      <Option key={node.drupal_internal__tid} value={node.drupal_internal__tid}>
+        {node.name}
+      </Option>
     )
+  })
+
+  // title search filter
+  let filteredNewsEvents = data.allNodeNewsEvents.edges.filter(edge => {
+    return edge.node.title.toLowerCase().indexOf(search.toLowerCase()) !== -1
+  })
+
+  // category filter
+  if (category) {
+    filteredNewsEvents = filteredNewsEvents.filter(item => {
+      return item.node.relationships.field_category
+        .map(({ drupal_internal__tid }) => drupal_internal__tid)
+        .includes(category)
+    })
   }
+
+  // pagination
+  const pagedNewsEvents = paginate(filteredNewsEvents)
+
+  const displayItems = items => {
+    if (items.length > 0) {
+      const itemsToDisplay = []
+      items.map(item => {
+        itemsToDisplay.push(
+          <NewsCard
+            key={item.node.drupal_internal__nid.toString()}
+            node={item.node}
+          />
+        )
+      })
+      return itemsToDisplay
+    } else {
+      return 'No items found'
+    }
+  }
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>
+          {siteTitle} - {pageTitle}
+        </title>
+      </Helmet>
+
+      <Breadcrumbs />
+
+      <header className="full-width-header">
+        <h1>{pageTitle}</h1>
+
+        <section className="filters">
+          <section className="category-filter">
+            <Select
+              placeholder="Category"
+              style={{ width: '100%' }}
+              onChange={value => updateCategory(value)}
+              allowClear
+            >
+              {categories}
+            </Select>
+          </section>
+
+          <section className="search-filter">
+            <Search
+              placeholder="Title"
+              allowClear
+              enterButton
+              onChange={e => updateSearch(e.target.value)}
+              onSearch={value => updateSearch(value)}
+            />
+          </section>
+        </section>
+
+        <section className="filter-results">
+          <Pagination
+            total={filteredNewsEvents.length}
+            current={page}
+            showTotal={(total, range) =>
+              `${range[0]}-${range[1]} of ${total} items`
+            }
+            pageSize={cardsPerPage}
+            className="pagination"
+            onChange={page => setPage(page)}
+          />
+        </section>
+      </header>
+
+      <main className="news-events-page">
+        <section className="news-grid">{displayItems(pagedNewsEvents)}</section>
+      </main>
+    </Layout>
+  )
 }
 
 export default AllNewsEventsPage
